ef00449390781a435dc84d81aede8589ed048b73
chimir <zhe9191@yandex.ru>
2017-08-24 19:41:47 +0200

    убрал лишний массив, русские названия перенес в массив ADS_TYPE

--- js/map.js
+++ js/map.js
@@ -12,9 +12,18 @@ var ADS_TITLE = [
 ];
 
 var ADS_TYPE = [
-  'flat',
-  'house',
-  'bungalo'
+  {
+    'en': 'flat',
+    'ru': 'квартира'
+  },
+  {
+    'en': 'house',
+    'ru': 'дом'
+  },
+  {
+    'en': 'bungalo',
+    'ru': 'бунгало'
+  }
 ];
 
 var ADS_CHECKIN = [
@@ -162,16 +171,10 @@ for (i = 0; i < adsFeatures.length; i++) {
 var fillLodge = function (data) {
   var adsElement = similarLodgeTemplate.cloneNode(true);
 
-  var adsTypeTranslate = {
-    'flat': 'квартира',
-    'house': 'дом',
-    'bungalo': 'бунгало'
-  };
-
   adsElement.querySelector('.lodge__title').textContent = data.offer.title;
   adsElement.querySelector('.lodge__address').textContent = data.location.x + ', ' + data.location.y;
   adsElement.querySelector('.lodge__price').innerHTML = data.offer.price + '&#x20bd;/ночь';
-  adsElement.querySelector('.lodge__type').textContent = adsTypeTranslate[data.offer.type];
+  adsElement.querySelector('.lodge__type').textContent = data.offer.type.ru;
   adsElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + data.offer.guests + ' гостей в ' + data.offer.rooms + ' комнатах';
   adsElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + data.offer.checkin + ' , выезд до ' + data.offer.checkout;
   adsElement.querySelector('.lodge__features').appendChild(fragment);
