e7560fe6ca446e4abb748696125e5ffddd8a1de0
chimir <zhe9191@yandex.ru>
2017-09-03 18:59:11 +0200

    Реализация перетаскивания метки и заполнения поля адреса

--- js/map.js
+++ js/map.js
@@ -7,10 +7,13 @@
   similarDialog.classList.add('hidden'); // Скрываем диологовое окно по умолчанию.
 
   var openDialog = function (evt) {
-    window.pin.activatePin(evt);
-    similarDialog.classList.remove('hidden');
+    // Проверяем, что клик был не по метке с классом pin__main.
+    if (!evt.target.classList.contains('pin__main') && !evt.target.parentNode.classList.contains('pin__main')) {
+      window.pin.activatePin(evt);
+      similarDialog.classList.remove('hidden');
 
-    window.card.replaceDialog(evt);
+      window.card.replaceDialog(evt);
+    }
   };
 
   var closeDialog = function () {
@@ -40,6 +43,76 @@
   // Закрытие диалогового окна при нажатии Ssc.
   document.addEventListener('keydown', function (evt) {
     window.util.isEscEvent(evt, closeDialog);
+  });
+
+  /**
+   * =========================
+   * #16 максимум подвижности
+   * =========================
+   */
+
+  var pinMain = similarPin.querySelector('.pin__main');
+  var addressField = document.querySelector('#address');
+
+  var pinWidth = 75;
+  var pinHeight = 94;
+
+  // Координаты острия метки.
+  var getPinGadPosition = function (x, y) {
+    var pinGadPositionX = x + pinWidth / 2;
+    var pinGadPositionY = y + pinHeight;
+
+    return 'x: ' + pinGadPositionX + ', y: ' + pinGadPositionY;
+  };
+
+  addressField.readOnly = true;
+  addressField.value = getPinGadPosition(pinMain.offsetLeft, pinMain.offsetTop);
+
+  // Перемещение метки.
+  pinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    // Начальные координаты мышки.
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    // Удержание мышки.
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var pinPosition = {
+        x: (pinMain.offsetLeft - shift.x),
+        y: (pinMain.offsetTop - shift.y)
+      };
+
+      pinMain.style.top = pinPosition.y + 'px';
+      pinMain.style.left = pinPosition.x + 'px';
+
+      addressField.value = getPinGadPosition(pinPosition.x, pinPosition.y);
+    };
+
+    // Отпускание мышки.
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
 
