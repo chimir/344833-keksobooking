3558a8cf65d7b4c6dfad29ba8f5e7c13af256da6
chimir <zhe9191@yandex.ru>
2017-09-14 16:44:18 +0200

    отображение типа жилья

--- js/card.js
+++ js/card.js
@@ -2,6 +2,11 @@
 (function () {
   var similarLodgeTemplate = document.querySelector('#lodge-template').content;
   var similarDialog = document.querySelector('.dialog');
+  var adsTypeTranslate = {
+    'flat': 'квартира',
+    'house': 'дом',
+    'bungalo': 'бунгало'
+  };
 
   // Список доступных удобств.
   var getFeatures = function (arr) {
@@ -24,7 +29,7 @@
       adsElement.querySelector('.lodge__title').textContent = data.offer.title;
       adsElement.querySelector('.lodge__address').textContent = data.location.x + ', ' + data.location.y;
       adsElement.querySelector('.lodge__price').innerHTML = data.offer.price + '&#x20bd;/ночь';
-      adsElement.querySelector('.lodge__type').textContent = data.offer.type.ru;
+      adsElement.querySelector('.lodge__type').textContent = adsTypeTranslate[data.offer.type];
       adsElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + data.offer.guests + ' гостей в ' + data.offer.rooms + ' комнатах';
       adsElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + data.offer.checkin + ' , выезд до ' + data.offer.checkout;
       adsElement.querySelector('.lodge__features').appendChild(getFeatures(data.offer.features));
